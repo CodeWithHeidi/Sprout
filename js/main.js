ec1b4d44cb86133f990c95f448b4308946547bbf
CodeWithHeidi <pulsarred00@gmail.com>
2023-11-28 08:39:38 -0600

    sdfsdfsdf

--- js/main.js
+++ js/main.js
@@ -93,3 +93,31 @@
     
 })(jQuery);
 
+window.onload = function () { 
+    let slides =  
+        document.getElementsByClassName('carousel-item'); 
+  
+    function addActive(slide) { 
+        slide.classList.add('active'); 
+    } 
+  
+    function removeActive(slide) { 
+        slide.classList.remove('active'); 
+    } 
+  
+    addActive(slides[0]); 
+    setInterval(function () { 
+        for (let i = 0; i < slides.length; i++) { 
+            if (i + 1 == slides.length) { 
+                addActive(slides[0]); 
+                setTimeout(removeActive, 350, slides[i]); 
+                break; 
+            } 
+            if (slides[i].classList.contains('active')) { 
+                setTimeout(removeActive, 350, slides[i]); 
+                addActive(slides[i + 1]); 
+                break; 
+            } 
+        } 
+    }, 1500); 
+};
